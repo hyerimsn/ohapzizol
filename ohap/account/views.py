44d8d76e3b4dbe28db0699cb6497b09f4cf011d8
k1myum1n <dbals757@likelion.org>
2020-06-03 17:41:29 +0900

    드디어 됐다 로그인8ㅅ8

--- ohap/account/views.py
+++ ohap/account/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
 
+def home(request):
+    return render(request,'index.html')
+
+def sign_up(request):
+    if request.method == "POST":
+
+        sign_up_form = UserCreationForm(request.POST)
+
+        if sign_up_form.is_valid:
+            sign_up_form.save()
+            return redirect('home')
+
+        else:
+            return redirect('sign_up')
+
+    sign_up_form = UserCreationForm()
+    return render(request,'registration/sign_up.html',{'sign_up_form':sign_up_form})
 
 # Create your views here.
--- ohap/ohap/urls.py
+++ ohap/ohap/urls.py
@@ -14,20 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView,LogoutView #login
 from django.urls import path
 from info.views import info
 from django.conf import settings
 from django.conf.urls.static import static
 from first import views
+from account import views as account_views #account view
 from comment.views import dongyeop,create_comment,delete_comment,create_recomment
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home ,name='home'),
+    path('account/sign_up/',account_views.sign_up,name="sign_up"), #회원가입
+    path('account/login/',LoginView.as_view(),name="login"), #로그인
+    path('account/logout/',LogoutView.as_view(),name="logout"), #로그아웃
     path('dongyeop/<int:temp_id>', dongyeop, name="dongyeop"),
     path('create_comment/<int:temp_id>',create_comment , name="create_comment"),
     path('delete_comment/<int:com_id>/<int:temp_id>',delete_comment , name="delete_comment"),
     path('create_recomment/<int:temp_id>',create_recomment , name="create_recomment"),
-    path('admin/', admin.site.urls),
     path('info/', info, name="info"),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
